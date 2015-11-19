842e6b1633a4362dad4ea2d8c2992e6674040d7f
Marius Gheorghe <gmarius@gmail.com>
2015-11-19 17:59:04 +0200

    updated db backup script to include hh-mm-ss in filenames

--- alldatabasesbackup.sql
+++ alldatabasesbackup.sql
@@ -2,17 +2,20 @@
 DECLARE @name VARCHAR(50) -- database name  
 DECLARE @path VARCHAR(256) -- path for backup files  
 DECLARE @fileName VARCHAR(256) -- filename for backup  
-DECLARE @fileDate VARCHAR(20) -- used for file name
+DECLARE @fileDate VARCHAR(60) -- used for file name
 
  
 -- specify database backup directory
-SET @path = 'C:\autodump\'  
+SET @path = 'C:\autodump2\'  
 
  
 -- specify filename format
-SELECT @fileDate = CONVERT(VARCHAR(20),GETDATE(),112) 
+SELECT @fileDate = CONVERT(VARCHAR(60),GETDATE(),20) 
 
- 
+SELECT @fileDate = REPLACE (@fileDate ,":", "-")
+
+
+
 DECLARE db_cursor CURSOR FOR  
 SELECT name 
 FROM master.dbo.sysdatabases 
